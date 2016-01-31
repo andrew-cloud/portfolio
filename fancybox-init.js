f608ee3c8f31afa32a1025a01e13ec5a93fe9f9e
andrew-cloud <cloud.a@husky.neu.edu>
2016-01-31 17:54:00 -0500

    fleshed out europe portfolio page

--- fancybox-init.js
+++ fancybox-init.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
 		openEffect	: 'elastic',
 		closeEffect	: 'elastic'
 	});
+$(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+});
 });
